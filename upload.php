ef04aba7382b1f1613049f3800074fb26583c85c
davidlinc1 <daveclark20@gmail.com>
2018-10-28 04:43:10 -0700

    File uploading

--- upload.php
+++ upload.php
@@ -2,6 +2,8 @@
 $target_dir = "img/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
+$fileExists = 0;
+
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
@@ -17,10 +19,10 @@ if(isset($_POST["submit"])) {
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
-    $uploadOk = 0;
+    $fileExists = 1;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 8000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -32,13 +34,17 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+	echo json_encode(array("type" => "error", "message" => "Sorry, there was an error uploading your file."));
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    	if ($fileExists == 1) {
+    		echo json_encode(array("type" => "success", "info" => "Filename already exists. Using existing resource", "file" => basename( $_FILES["fileToUpload"]["name"])));
+    	} else {
+    		echo json_encode(array("type" => "success", "file" => basename( $_FILES["fileToUpload"]["name"])));
+    	}
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo '{"type":"error","message" : "Sorry, there was an error uploading your file."}';
     }
 }
 ?>

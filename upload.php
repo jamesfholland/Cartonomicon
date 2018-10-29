e1bcc7ecb0c71dd31f541ffce7ae29625b6125e2
davidlinc1 <daveclark20@gmail.com>
2018-10-29 03:32:01 -0700

    Map linking, file uploading, and basic element storage

--- upload.php
+++ upload.php
@@ -9,27 +9,27 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        // echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        // echo "File is not an image.";
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    // echo "Sorry, file already exists.";
     $fileExists = 1;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 8000000) {
-    echo "Sorry, your file is too large.";
+if ($_FILES["fileToUpload"]["size"] > 15000000) {
+    // echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -44,7 +44,7 @@ if ($uploadOk == 0) {
     		echo json_encode(array("type" => "success", "file" => basename( $_FILES["fileToUpload"]["name"])));
     	}
     } else {
-        echo '{"type":"error","message" : "Sorry, there was an error uploading your file."}';
+        echo 'Nope.';
     }
 }
 ?>
